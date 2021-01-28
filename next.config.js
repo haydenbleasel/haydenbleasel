7986355c170c2a979aa9adfc6399d510f3530f08
Hayden Bleasel <hello@haydenbleasel.com>
2021-01-28 11:10:29 +1100

    Add Preact, PWA and bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,12 +1,31 @@
-module.exports = {
-    i18n: {
-        locales: ['en-US'],
-        defaultLocale: 'en-US'
-    },
-    images: {
-        domains: [
-            'prismic-io.s3.amazonaws.com',
-            'i.scdn.co',
+const withPlugins = require('next-compose-plugins');
+const withPWA = require('next-pwa');
+const withPreact = require('next-plugin-preact');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true'
+});
+
+module.exports = withPlugins(
+    [
+        withPreact,
+        [
+            withPWA,
+            {
+                pwa: { dest: 'public', disable: process.env.NODE_ENV === 'development' }
+            }
         ],
-    },
-}
+        [withBundleAnalyzer],
+    ],
+    {
+        i18n: {
+            locales: ['en-US'],
+            defaultLocale: 'en-US'
+        },
+        images: {
+            domains: [
+                'prismic-io.s3.amazonaws.com',
+                'i.scdn.co',
+            ],
+        },
+    }
+);
