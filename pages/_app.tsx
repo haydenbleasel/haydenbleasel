fd7002a92f592a0354ce2e6b15662f07e1d7e43a
Hayden Bleasel <hello@haydenbleasel.com>
2021-01-29 13:51:08 +1100

    Move HTML lang to _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,45 +4,41 @@ import Head from 'next/head';
 
 import './styles.css';
 
-export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-    return (
-        <>
-            <Head>
-                <html lang="en" />
+export default ({ Component, pageProps }: AppProps): JSX.Element => (
+    <>
+        <Head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
 
-                <meta charSet="utf-8" />
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="application-name" content="Hayden Bleasel" />
+            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+            <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
 
-                <meta name="application-name" content="Hayden Bleasel" />
-                <meta name="apple-mobile-web-app-capable" content="yes" />
-                <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-                <meta name="apple-mobile-web-app-title" content="Hayden Bleasel" />
+            <meta name="format-detection" content="telephone=no" />
+            <meta name="mobile-web-app-capable" content="yes" />
+            <meta name="msapplication-config" content="/browserconfig.xml" />
+            <meta name="msapplication-TileColor" content="#2B5797" />
+            <meta name="msapplication-tap-highlight" content="no" />
+            <meta name="theme-color" content="#0C0D15" />
+            <link rel="manifest" href="/manifest.json" />
 
-                <meta name="format-detection" content="telephone=no" />
-                <meta name="mobile-web-app-capable" content="yes" />
-                <meta name="msapplication-config" content="/browserconfig.xml" />
-                <meta name="msapplication-TileColor" content="#2B5797" />
-                <meta name="msapplication-tap-highlight" content="no" />
-                <meta name="theme-color" content="#0C0D15" />
-                <link rel="manifest" href="/manifest.json" />
+            {[57, 60, 72, 76, 114, 120, 144, 152, 180].map((size) => (
+                <link rel="apple-touch-icon" sizes={`${size}x${size}`} href={`/apple-touch-icon-${size}x${size}.png`} />
+            ))}
 
-                {[57, 60, 72, 76, 114, 120, 144, 152, 180].map((size) => (
-                    <link rel="apple-touch-icon" sizes={`${size}x${size}`} href={`/apple-touch-icon-${size}x${size}.png`} />
-                ))}
+            {[16, 32, 194].map((size) => (
+                <link rel="icon" type="image/png" sizes={`${size}x${size}`} href={`/favicon-${size}x${size}.png`} />
+            ))}
 
-                {[16, 32, 194].map((size) => (
-                    <link rel="icon" type="image/png" sizes={`${size}x${size}`} href={`/favicon-${size}x${size}.png`} />
-                ))}
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0c0d15" />
 
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0c0d15" />
+            {[450, 650].map((weight) => (
+                <link rel="preload" href={`/fonts/UniversalSans-v1-2-0-50-0-721212122121-01-${weight}.woff`} as="font" type="font/woff" />
+            ))}
 
-                {[450, 650].map((weight) => (
-                    <link rel="preload" href={`/fonts/UniversalSans-v1-2-0-50-0-721212122121-01-${weight}.woff`} as="font" type="font/woff" />
-                ))}
-
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
-}
+        </Head>
+        <Component {...pageProps} />
+    </>
+);
