5ace09260bbe715966792d5e78ae723eb4cd06d3
Hayden Bleasel <hello@haydenbleasel.com>
2021-05-24 21:44:02 +1000

    Move Fade into components

--- components/Project/index.tsx
+++ components/Project/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import { Fade } from 'react-awesome-reveal';
 import Link from '../Link';
 import styles from './role.module.css';
 
@@ -13,21 +14,23 @@ type IProject = {
 
 const Project = ({ date, name, role, link, children }: IProject) => (
   <div className={styles.role}>
-    <Image
-      src={`/images/projects/${name.toLowerCase()}.png`}
-      layout="responsive"
-      width={1312}
-      height={600}
-    />
-    <div className={styles.meta}>
-      <div className={styles.summary}>
-        <p className="verySmall">{date}</p>
-        <h2 className="h2Sans">{name}</h2>
-        <p className="h2Serif">{role}</p>
-        <Link href={link}>Visit the website</Link>
+    <Fade triggerOnce>
+      <Image
+        src={`/images/projects/${name.toLowerCase()}.png`}
+        layout="responsive"
+        width={1312}
+        height={600}
+      />
+      <div className={styles.meta}>
+        <div className={styles.summary}>
+          <p className="verySmall">{date}</p>
+          <h2 className="h2Sans">{name}</h2>
+          <p className="h2Serif">{role}</p>
+          <Link href={link}>Visit the website</Link>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Fade>
   </div>
 );
 
--- components/Role/index.tsx
+++ components/Role/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import { Fade } from 'react-awesome-reveal';
 import styles from './role.module.css';
 
 type IRole = {
@@ -11,20 +12,22 @@ type IRole = {
 
 const Role = ({ date, title, company, children }: IRole) => (
   <div className={styles.role}>
-    <Image
-      src={`/images/work/${company.toLowerCase()}.png`}
-      layout="responsive"
-      width={1312}
-      height={600}
-    />
-    <div className={styles.meta}>
-      <div className={styles.summary}>
-        <p className="verySmall">{date}</p>
-        <h2 className="h2Sans">{title}</h2>
-        <p className="h2Serif">{company}</p>
+    <Fade triggerOnce>
+      <Image
+        src={`/images/work/${company.toLowerCase()}.png`}
+        layout="responsive"
+        width={1312}
+        height={600}
+      />
+      <div className={styles.meta}>
+        <div className={styles.summary}>
+          <p className="verySmall">{date}</p>
+          <h2 className="h2Sans">{title}</h2>
+          <p className="h2Serif">{company}</p>
+        </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </Fade>
   </div>
 );
 
