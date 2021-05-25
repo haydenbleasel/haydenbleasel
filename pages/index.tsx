bed3df8dbce95849abe7c84051f9da4ccad9138f
Hayden Bleasel <hello@haydenbleasel.com>
2021-05-25 19:24:07 +1000

    Misc fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import Image from "next/image";
 import Layout from "../components/layout";
 import Link from "../components/link";
 import Client from "../components/client";
