43b5f4a03f2a9957d946d9c406a65bc9c1e96aa7
Hayden Bleasel <hello@haydenbleasel.com>
2021-06-07 15:29:46 +1000

    Create table of contents, misc layout improvements

--- pages/case-studies/[uid].tsx
+++ pages/case-studies/[uid].tsx
@@ -1,6 +1,8 @@
 import type { GetStaticProps, GetStaticPaths } from 'next';
-import { useState } from 'react';
 import type { FormEvent } from 'react';
+import { useState } from 'react';
+import Image from 'next/image';
+import slugify from 'slugify';
 import Layout from "../../components/layout";
 import Form from "../../components/form";
 import Section from "../../components/section";
@@ -22,6 +24,26 @@ async function fetchPrismicData({ uid }) {
   const { data } = await queryAt('my.case_study.uid', uid);
 
   return data;
+}
+
+function filterHeadings({ type }) {
+  return type.startsWith('heading');
+}
+
+function slugElement({ text }) {
+  return slugify(text, { lower: true, strict: true });
+}
+
+function createTableElement({ element }) {
+  const id = slugElement(element);
+
+  console.log(element, 'x');
+
+  return (
+    <div className={`paragraphSans ${styles[element.type]}`}>
+      <a href={`#${id}`}>{element.text}</a>
+    </div>
+  );
 }
 
 const CaseStudy = ({ uid, settings }: ICaseStudy) => {
@@ -64,9 +86,27 @@ const CaseStudy = ({ uid, settings }: ICaseStudy) => {
       settings={settings}
     >
       <Title title={data.title} />
+
+      <Image src={data.cover.url} width={1312} height={600} alt={data.title} layout="responsive" />
       
       <Section>
-        <div style={{ gridColumn: '1 / 13' }} dangerouslySetInnerHTML={{ __html: richtext(data.content) }} />
+        <div className={styles.content} dangerouslySetInnerHTML={{ __html: richtext(data.content, false, {
+          heading1: ({ children, element }) => <h1 id={slugElement(element)} className="h1Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          heading2: ({ children, element }) => <h2 id={slugElement(element)} className="h2Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          heading3: ({ children, element }) => <h3 id={slugElement(element)} className="h3Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          heading4: ({ children, element }) => <h4 id={slugElement(element)} className="h4Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          heading5: ({ children, element }) => <h5 id={slugElement(element)} className="h5Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          heading6: ({ children, element }) => <h6 id={slugElement(element)} className="h6Sans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+          paragraph: ({ children }) => <p className="paragraphSans" dangerouslySetInnerHTML={{ __html: children.join('')}} />,
+        }) }} />
+        <div className={styles.table} dangerouslySetInnerHTML={{ __html: richtext(data.content.filter(filterHeadings), false, {
+          heading1: createTableElement,
+          heading2: createTableElement,
+          heading3: createTableElement,
+          heading4: createTableElement,
+          heading5: createTableElement,
+          heading6: createTableElement,
+        })}} />
       </Section>
     </Layout>
   ) : (
