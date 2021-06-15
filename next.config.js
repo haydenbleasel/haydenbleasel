402723507be6bbbd4651cbf0811483ba9f13cabe
Hayden Bleasel <hello@haydenbleasel.com>
2021-06-16 02:44:23 +1000

    Upgrade to Next 11

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 });
 
 const config = {
-  future: {
-    webpack5: true,
-  },
   images: {
     deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     domains: [
