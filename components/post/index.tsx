7f58155a1d0112b30010768a487e6747931a9da1
Hayden Bleasel <hello@haydenbleasel.com>
2021-06-16 20:32:23 +1000

    Simplify structure of Post component

--- components/post/index.tsx
+++ components/post/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import type { HTMLAttributes } from "react";
 import Link from "../link";
 import Skeleton from "../skeleton";
 import styles from "./post.module.css";
@@ -12,7 +13,7 @@ type PostProps = {
   featured?: boolean;
   focus?: string;
   compact?: boolean;
-};
+} & HTMLAttributes<HTMLAnchorElement>;
 
 const Post = ({
   image,
@@ -23,35 +24,34 @@ const Post = ({
   featured = false,
   compact = false,
   focus,
+  ...props
 }: PostProps) => (
-  <div className={styles.post}>
-    <Link href={link}>
-      {!!image && !compact && (
-        <div className={styles.image}>
-          <Skeleton>
-            <Image
-              layout="responsive"
-              width={featured ? 1128 : 742}
-              height={featured ? 600 : 395}
-              alt={title}
-              src={image.url}
-              quality={100}
-              objectFit="cover"
-              objectPosition={focus}
-              priority={featured}
-            />
-          </Skeleton>
-        </div>
-      )}
-      <div className={styles.meta}>
-        {!!caption && <small className="small grey">{caption}</small>}
-        <h2 className={compact ? "paragraphSans" : "h4Sans"}>{title}</h2>
-        {!!description && !compact && (
-          <p className="paragraphSans grey">{description}</p>
-        )}
+  <Link href={link} className={styles.post} {...props}>
+    {!!image && !compact && (
+      <div className={styles.image}>
+        <Skeleton>
+          <Image
+            layout="responsive"
+            width={featured ? 1128 : 742}
+            height={featured ? 600 : 395}
+            alt={title}
+            src={image.url}
+            quality={100}
+            objectFit="cover"
+            objectPosition={focus}
+            priority={featured}
+          />
+        </Skeleton>
       </div>
-    </Link>
-  </div>
+    )}
+    <div className={styles.meta}>
+      {!!caption && <small className="small grey">{caption}</small>}
+      <h2 className={compact ? "paragraphSans" : "h4Sans"}>{title}</h2>
+      {!!description && !compact && (
+        <p className="paragraphSans grey">{description}</p>
+      )}
+    </div>
+  </Link>
 );
 
 export default Post;
