b1d86ef6a52c834b3d62ff7241f871597851f9d0
Hayden Bleasel <hello@haydenbleasel.com>
2021-06-17 19:50:17 +1000

    Simplify list of roles

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -1,10 +1,13 @@
 import type { GetStaticProps, NextPage } from 'next';
+import Link from '../../components/link';
 import Layout from "../../components/layout";
 import Section from "../../components/section";
 import Card from "../../components/card";
 import Title from "../../components/title";
 import Outlink from "../../components/outlink";
 import { queryAt } from "../../utils/prismic";
+import jellypepper from '../../data/jellypepper.json';
+import styles from './work.module.css';
 
 type IWork = {
   data: {
@@ -13,6 +16,7 @@ type IWork = {
     hero_title: PrismicRichText;
     action_cta: string;
     action_link: PrismicLink;
+    jellypepper_title: string;
   };
   settings: PrismicSettings;
   roles: {
@@ -21,11 +25,64 @@ type IWork = {
   }[];
 }
 
+type IClient = {
+  id: string;
+  name: string;
+  projects: {
+    name: string;
+    link?: string;
+  }[];
+  roles: string[];
+}
+
+function Client({ id, name, projects, roles }: IClient) {
+  projects[0].name = projects[0].name.charAt(0).toUpperCase() + projects[0].name.slice(1);
+
+  return (
+    <div id={id} key={id} className={styles.client}>
+      <p>
+        <span className="small">
+          {roles?.join(' and ')},&nbsp;
+        </span>
+        <span className="smallSerif">{name}</span>
+      </p>
+      <p className="small">
+        {projects.map((project, index) => {
+          const projectDescription = project.link ? (
+            <Link href={project.link}>{project.name}</Link>
+          ) : (
+            project.name
+          );
+
+          let projectDivider = <>, </>;
+
+          if (index === projects.length - 1) {
+            projectDivider = <>.</>;
+          } else if (index === projects.length - 2) {
+            projectDivider = <> and </>;
+          }
+
+          return (
+            <>
+              {projectDescription}
+              {projectDivider}
+            </>
+          );
+        })}
+      </p>
+    </div>
+  )
+}
+
 function sortRoles(a, b) {
   const dateA = a.data.date.split(' — ')[0];
   const dateB = b.data.date.split(' — ')[0];
   
   return new Date(dateA) < new Date(dateB) ? 1 : -1;
+}
+
+function sortById(a: any, b: any) {
+  return a.id > b.id ? 1 : -1;
 }
 
 const Work: NextPage<IWork> = ({ data, roles, settings }) => (
@@ -50,6 +107,17 @@ const Work: NextPage<IWork> = ({ data, roles, settings }) => (
         </Card>
       </Section>
     ))}
+
+    <Section>
+      <p className={styles.jellypepperTitle}>{data.jellypepper_title}</p>
+      <div className={styles.clients}>
+        <div className={styles.listHeader}>
+          <p className="small">Client</p>
+          <p className="small">Project</p>
+        </div>
+        {jellypepper.sort(sortById).map(Client)}
+      </div>
+    </Section>
 
     <Section>
       <Outlink
