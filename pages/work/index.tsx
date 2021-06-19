1c3c7b9204ec193ba8f0e87305d65d5eb5898040
Hayden Bleasel <hello@haydenbleasel.com>
2021-06-20 00:29:00 +1000

    Finish work page

--- pages/work/index.tsx
+++ pages/work/index.tsx
@@ -5,8 +5,7 @@ import Section from "../../components/section";
 import Card from "../../components/card";
 import Title from "../../components/title";
 import Outlink from "../../components/outlink";
-import { queryAt } from "../../utils/prismic";
-import jellypepper from '../../data/jellypepper.json';
+import { queryAt, richtext } from "../../utils/prismic";
 import styles from './work.module.css';
 
 type IWork = {
@@ -16,62 +15,20 @@ type IWork = {
     hero_title: PrismicRichText;
     action_cta: string;
     action_link: PrismicLink;
+    other_intro: string;
     jellypepper_intro: string;
+    jellypepper_roles: {
+      role_date: string;
+      role_title: string;
+      role_company: string;
+      role_description: PrismicRichText;
+    }[];
   };
   settings: PrismicSettings;
   roles: {
     uid: string;
     data: PrismicRole;
   }[];
-}
-
-type IClient = {
-  id: string;
-  name: string;
-  projects: {
-    name: string;
-    link?: string;
-  }[];
-  roles: string[];
-}
-
-function Client({ id, name, projects, roles }: IClient) {
-  projects[0].name = projects[0].name.charAt(0).toUpperCase() + projects[0].name.slice(1);
-
-  return (
-    <div id={id} key={id} className={styles.client}>
-      <p>
-        <span className="smallSans">
-          {roles?.join(' and ')},&nbsp;
-        </span>
-        <span className="smallSerif">{name}</span>
-      </p>
-      <p className="smallSans">
-        {projects.map((project, index) => {
-          const projectDescription = project.link ? (
-            <Link href={project.link}>{project.name}</Link>
-          ) : (
-            project.name
-          );
-
-          let projectDivider = <>, </>;
-
-          if (index === projects.length - 1) {
-            projectDivider = <>.</>;
-          } else if (index === projects.length - 2) {
-            projectDivider = <> and </>;
-          }
-
-          return (
-            <span key={index}>
-              {projectDescription}
-              {projectDivider}
-            </span>
-          );
-        })}
-      </p>
-    </div>
-  )
 }
 
 function sortRoles(a, b) {
@@ -81,8 +38,29 @@ function sortRoles(a, b) {
   return new Date(dateA) < new Date(dateB) ? 1 : -1;
 }
 
-function sortById(a: any, b: any) {
-  return a.id > b.id ? 1 : -1;
+function sortJellypepperRoles(a, b) {
+  const dateA = a.role_date;
+  const dateB = b.role_date;
+  
+  return new Date(dateA) < new Date(dateB) ? 1 : -1;
+}
+
+function Client({
+  date,
+  title,
+  subtitle,
+  description,
+}) {
+  return (
+    <div className={styles.client}>
+      <p className="smallSans">{date}</p>
+      <p>
+        <span className="paragraphSans">{title},&nbsp;</span>
+        <span className="paragraphSerif">{subtitle}</span>
+      </p>
+      <p className="smallSans" dangerouslySetInnerHTML={{ __html: richtext(description, true) }} />
+    </div>
+  );
 }
 
 const Work: NextPage<IWork> = ({ data, roles, settings }) => (
@@ -109,13 +87,42 @@ const Work: NextPage<IWork> = ({ data, roles, settings }) => (
     ))}
 
     <Section>
-      <p className={styles.jellypepperTitle}>{data.jellypepper_intro}</p>
+      <p className={styles.subtitle}>{data.other_intro}</p>
       <div className={styles.clients}>
         <div className={styles.listHeader}>
-          <p className="smallSans">Client</p>
-          <p className="smallSans">Project</p>
+          <p className="smallSans">Date</p>
+          <p className="smallSans">Role</p>
+          <p className="smallSans">Description</p>
         </div>
-        {jellypepper.sort(sortById).map(Client)}
+        {roles.filter(({ data }) => !data.featured).sort(sortRoles).map((role) => (
+          <Client
+            key={role.uid}
+            date={role.data.date}
+            title={role.data.title}
+            subtitle={role.data.description}
+            description={role.data.content}
+          />
+        ))}
+      </div>
+    </Section>
+
+    <Section>
+      <p className={styles.subtitle}>{data.jellypepper_intro}</p>
+      <div className={styles.clients}>
+        <div className={styles.listHeader}>
+          <p className="smallSans">Date</p>
+          <p className="smallSans">Client</p>
+          <p className="smallSans">Description</p>
+        </div>
+        {data.jellypepper_roles.sort(sortJellypepperRoles).map((role) => (
+          <Client
+            key={role.role_company}
+            date={role.role_date}
+            title={role.role_title}
+            subtitle={role.role_company}
+            description={role.role_description}
+          />
+        ))}
       </div>
     </Section>
 
