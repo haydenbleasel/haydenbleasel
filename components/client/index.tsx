2e6e74fea2f1470a5361450720b5ec41c6644d83
Hayden Bleasel <hello@haydenbleasel.com>
2021-07-20 07:47:10 +1000

    Use require for importing logos

--- components/client/index.tsx
+++ components/client/index.tsx
@@ -9,25 +9,27 @@ type IClient = {
   content: string;
 };
 
-const Client = ({ element, content }: IClient) => (
-  <div className={styles.clientWrapper}>
-    <Link href={element.data} className={styles.client}>
-      <Image
-        layout="fixed"
-        height={32}
-        width={32}
-        alt={content}
-        src={`/logos/${content
-          .replace(/ /g, "")
-          .replace(/\//g, "")
-          .toLowerCase()}.svg`}
-        quality={100}
-        objectFit="contain"
-        priority
-      />&nbsp;
-      <span className={styles.clientName}>{content}</span>
-    </Link>
-  </div>
-);
+const Client = ({ element, content }: IClient) => {
+  const filename = content.replace(/ /g, "").replace(/\//g, "").toLowerCase();
+  const logo = require(`./logos/${filename}.svg`);
+
+  return (
+    <div className={styles.clientWrapper}>
+      <Link href={element.data} className={styles.client}>
+        <Image
+          layout="fixed"
+          height={32}
+          width={32}
+          alt={content}
+          src={logo}
+          quality={100}
+          objectFit="contain"
+          priority
+        />&nbsp;
+        <span className={styles.clientName}>{content}</span>
+      </Link>
+    </div>
+  );
+}
 
 export default Client;
