e852fc77563d25d2c6d9486ef640ee720119a01d
Hayden Bleasel <hello@haydenbleasel.com>
2021-10-08 19:27:48 +1100

    Attempt to dodge FOUC issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -323,7 +323,7 @@ const Home: NextPage<any> = () => {
           <p>✌️</p>
         </section>
         <section className={styles.aside}>
-          <Image src={Spline} layout="fill" alt="A spline" objectFit="contain" objectPosition="top right" quality={100} placeholder="blur" />
+          <Image src={Spline} layout="fill" alt="A spline" objectFit="contain" objectPosition="top right" quality={100} placeholder="empty" />
         </section>
         <div className={styles.social}>
           {Object.values(social).map(Social)}
