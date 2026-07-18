8d187cf535174d0d9a2212124be0ec57fa3c38bd
Hayden Bleasel <hello@haydenbleasel.com>
2026-07-18 15:02:22 -0700

    Remove redundant oss app

--- apps/oss/lib/url.ts
+++
@@ -1,4 +0,0 @@
-const protocol = process.env.NODE_ENV === "production" ? "https" : "http";
-const origin = process.env.VERCEL_PROJECT_PRODUCTION_URL ?? "localhost:3003";
-
-export const url = `${protocol}://${origin}`;
